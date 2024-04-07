980258c3e8f4dda72ceb8370dcad8698079d1274
bye-shuvo <abuhasnatshuvo605@gmail.com>
2024-04-08 00:51:04 +0600

    Fix the js file

--- script.js
+++ script.js
@@ -1,88 +1,43 @@
-const click = new Audio("");
+const click = new Audio("click-47609.mp3");
 
-let n = document.querySelectorAll(".numitem")[0].innerText;
-n = Number.parseInt(n);
-
-let sign = document.querySelectorAll(".opitem")[2].innerText;
-sign = Number.parseInt(sign);
+let show = "";
 
 let buttonpress = () =>{
-
-    let nine = document.querySelector("#nine");
-    nine.addEventListener('click', () => {
-        document.querySelector(".top").append("9");
-    })
-    let eight = document.querySelector("#eight");
-    eight.addEventListener('click', () => {
-        document.querySelector(".top").append("8");
-        return 9;
-    })
-    let seven = document.querySelector("#seven");
-    seven.addEventListener('click', () => {
-        document.querySelector(".top").append("7");
-    })
-    let  six = document.querySelector("#six");
-    six.addEventListener('click', () => {
-        document.querySelector(".top").append("6");
-    })
-    let five= document.querySelector("#five");
-    five.addEventListener('click', () => {
-        document.querySelector(".top").append("5");
-    })
-    let four = document.querySelector("#four");
-    four.addEventListener('click', () => {
-        document.querySelector(".top").append("4");
-    })
-    let three = document.querySelector("#three");
-    three.addEventListener('click', () => {
-        document.querySelector(".top").append("3");
-    })
-    let two = document.querySelector("#two");
-    two.addEventListener('click', () => {
-        document.querySelector(".top").append("2");
-    })
-    let one = document.querySelector("#one");
-    one.addEventListener('click', () => {
-        document.querySelector(".top").append("1");
-    })
-    let zero = document.querySelector("#zero");
-    zero.addEventListener('click', () => {
-        document.querySelector(".top").append("0");
-    })
-    let dot = document.querySelector("#dot");
-    dot.addEventListener('click', () => {
-        document.querySelector(".top").append(".");
+    let buttons = document.querySelectorAll('.button');
+    Array.from(buttons).forEach((button) => {
+        button.addEventListener('click' , (e) => {
+            click.play();
+            if(e.target.innerHTML == '='){
+                show = eval(show);
+                document.querySelector('.top').innerText = show ;
+                document.querySelector('#display').innerText = show;
+            }
+            else{
+            show = show + e.target.innerHTML;
+            document.querySelector('.top').innerText = show ;
+            }
+        })
     })
 }
 
-let optionpress = () => {
-    document.getElementsByClassName('opitem')[2].addEventListener('click',() => {
-        document.querySelector(".top").append("+");
-    })
-    document.getElementsByClassName('opitem')[3].addEventListener('click',() => {
-        document.querySelector(".top").append("-");
-    })
-    document.getElementsByClassName('opitem')[4].addEventListener('click',() => {
-        document.querySelector(".top").append("×");
-    })
-    document.getElementsByClassName('opitem')[5].addEventListener('click',() => {
-        document.querySelector(".top").append("/");
-    })
-}
+buttonpress();
  
 let ac = () =>{
+    click.play();
     document.querySelector("#ac").addEventListener('click', () => {
-        document.querySelector(".top").innerText = "";
+        document.querySelector(".top").innerHTML = "";
+        show = "";
+        document.querySelector('#display').innerText = show;
     })
 }
 
-let calculation = () => {
-     document.querySelectorAll(".opitem")[6].addEventListener('click', () => {
-        let cal = n + sign + n;
-        document.getElementsByClassName("top")[0].append(cal);
-     })
-}
-calculation();
-optionpress();
-buttonpress();
 ac();
+
+let ans = () => {
+    document.querySelectorAll('.opitem')[0].addEventListener('click' ,() => {
+        document.querySelector('.top').innerText = show;
+        document.querySelector('#display').innerText = show;
+    })
+}
+
+ans();
